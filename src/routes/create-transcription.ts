ef06d9661216bb50a00e2b351ff2c4b7415445db
Fischer <fischerrobson@gmail.com>
2023-09-14 21:31:09 -0300

    routes implemented with openai integration: upload video, create transcription, generete completion

--- src/routes/create-transcription.ts
+++ src/routes/create-transcription.ts
@@ -1,6 +1,46 @@
 import { FastifyInstance } from 'fastify'
+import z from 'zod'
+import { createReadStream } from 'node:fs'
+import { prisma } from '../lib/prisma'
+import { openai } from '../lib/openai'
+
 export async function createTranscriptionRoute(app: FastifyInstance) {
   app.post('/videos/:id/transcription', async (req, res) => {
-    const { id } = req.params
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(req.params)
+
+    const bodySchema = z.object({
+      prompt: z.string(),
+    })
+
+    const { prompt } = bodySchema.parse(req.body)
+
+    const video = await prisma.video.findUniqueOrThrow({ where: { id } })
+    const videoPath = video.path
+
+    const audioReadStream = createReadStream(videoPath)
+
+    const response = await openai.audio.transcriptions.create({
+      file: audioReadStream,
+      model: 'whisper-1',
+      language: 'pt',
+      response_format: 'json',
+      temperature: 0,
+      prompt,
+    })
+
+    await prisma.video.update({
+      where: {
+        id,
+      },
+      data: {
+        transcription: response.text,
+      },
+    })
+
+    return response.text
   })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
 import { getPromptsRoute } from './routes/get-prompts'
 import { uploadVideoRoute } from './routes/upload-video'
+import { createTranscriptionRoute } from './routes/create-transcription'
+import { createAICompletionRoute } from './routes/create-ai-completion'
 
 const app = fastify()
 
+app.register(fastifyCors, {
+  origin: '*',
+})
+
 app.register(getPromptsRoute)
 app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(createAICompletionRoute)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server running on port 3333')
